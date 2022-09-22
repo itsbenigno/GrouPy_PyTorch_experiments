fe9bdcecf8a6fb729c830863b143af1256c6191b
Benigno Ansanelli <ansanellibenigno@gmail.com>
2022-09-22 12:23:41 +0200

    added first experiment

--- Models/All-CNN.py
+++ Models/All-CNN.py
@@ -51,7 +51,7 @@ class AllCNNC(nn.Module):
         x /= 8*8
 
 
-        return F.log_softmax(x, dim=1) #is it right?
+        return F.log_softmax(x, dim=1)
 
 
 
@@ -63,33 +63,27 @@ class P4AllCNNC(nn.Module):
     def __init__(self):
         super(P4AllCNNC, self).__init__()
         self.conv1 = P4ConvZ2(3, 48, 3, stride=1, padding=1)
-        self.conv1_bn = nn.BatchNorm2d(48*4)
+        self.conv1_bn = nn.BatchNorm3d(48)
         self.conv2 = P4ConvP4(48, 48, 3, stride=1, padding=1)
-        self.conv2_bn = nn.BatchNorm2d(48*4)
+        self.conv2_bn = nn.BatchNorm3d(48)
         self.conv3 = P4ConvP4(48, 48, 3, stride=2, padding=1)
-        self.conv3_bn = nn.BatchNorm2d(48*4)
+        self.conv3_bn = nn.BatchNorm3d(48)
         self.conv4 = P4ConvP4(48, 96, 3, stride=1, padding=1)
-        self.conv4_bn = nn.BatchNorm2d(96*4)
+        self.conv4_bn = nn.BatchNorm3d(96)
         self.conv5 = P4ConvP4(96, 96, 3, stride=1, padding=1)
-        self.conv5_bn = nn.BatchNorm2d(96*4)
+        self.conv5_bn = nn.BatchNorm3d(96)
         self.conv6 = P4ConvP4(96, 96, 3, stride=2, padding=1)
-        self.conv6_bn = nn.BatchNorm2d(96*4)
+        self.conv6_bn = nn.BatchNorm3d(96)
         self.conv7 = P4ConvP4(96, 96, 3, stride=1, padding=1)
-        self.conv7_bn = nn.BatchNorm2d(96*4)
+        self.conv7_bn = nn.BatchNorm3d(96)
         self.conv8 = P4ConvP4(96, 96, 1, stride=1, padding=0)
-        self.conv8_bn = nn.BatchNorm2d(96*4)
+        self.conv8_bn = nn.BatchNorm3d(96)
         self.conv9 = P4ConvP4(96, 10, 1, stride=1, padding=0)
-        self.conv9_bn = nn.BatchNorm2d(10*4)
+        self.conv9_bn = nn.BatchNorm3d(10)
 
     def forward(self, x):
         x = F.dropout(x, p=0.2, training=True)
-        x = self.conv1(x)
-        print("shape after first convolution",x.shape)
-        x = x.view(x.shape[0], x.shape[1]*4, 32, 32)
-        x = F.relu(self.conv1_bn(x))
-        x = x.view(x.shape[0], 4, -1, 32, 32)
-
-        """
+        x = F.relu(self.conv1_bn(self.conv1(x)))
         x = F.relu(self.conv2_bn(self.conv2(x)))
         x = F.relu(self.conv3_bn(self.conv3(x)))
         x = F.dropout(x, p=0.5, training=True)
@@ -100,14 +94,14 @@ class P4AllCNNC(nn.Module):
         x = F.relu(self.conv7_bn(self.conv7(x)))
         x = F.relu(self.conv8_bn(self.conv8(x)))
         x = F.relu(self.conv9_bn(self.conv9(x)))
-        """
+
         x = torch.sum(x, dim=-1)
         x = torch.sum(x, dim=-1)
         x = torch.sum(x, dim=-1)
         x /= 8*8 *4
 
 
-        return F.log_softmax(x, dim=1) #is it right?
+        return F.log_softmax(x, dim=1)
 
 
 class P4MAllCNNC(nn.Module):
@@ -118,23 +112,23 @@ class P4MAllCNNC(nn.Module):
     def __init__(self):
         super(P4MAllCNNC, self).__init__()
         self.conv1 = P4MConvZ2(3, 32, 3, stride=1, padding=1)
-        self.conv1_bn = nn.BatchNorm2d(32)
+        self.conv1_bn = nn.BatchNorm3d(32)
         self.conv2 = P4MConvP4M(32, 32, 3, stride=1, padding=1)
-        self.conv2_bn = nn.BatchNorm2d(32)
+        self.conv2_bn = nn.BatchNorm3d(32)
         self.conv3 = P4MConvP4M(32, 32, 3, stride=2, padding=1)
-        self.conv3_bn = nn.BatchNorm2d(32)
+        self.conv3_bn = nn.BatchNorm3d(32)
         self.conv4 = P4MConvP4M(32, 64, 3, stride=1, padding=1)
-        self.conv4_bn = nn.BatchNorm2d(64)
+        self.conv4_bn = nn.BatchNorm3d(64)
         self.conv5 = P4MConvP4M(64, 64, 3, stride=1, padding=1)
-        self.conv5_bn = nn.BatchNorm2d(64)
+        self.conv5_bn = nn.BatchNorm3d(64)
         self.conv6 = P4MConvP4M(64, 64, 3, stride=2, padding=1)
-        self.conv6_bn = nn.BatchNorm2d(64)
+        self.conv6_bn = nn.BatchNorm3d(64)
         self.conv7 = P4MConvP4M(64, 64, 3, stride=1, padding=1)
-        self.conv7_bn = nn.BatchNorm2d(64)
+        self.conv7_bn = nn.BatchNorm3d(64)
         self.conv8 = P4MConvP4M(64, 64, 1, stride=1, padding=0)
-        self.conv8_bn = nn.BatchNorm2d(64)
+        self.conv8_bn = nn.BatchNorm3d(64)
         self.conv9 = P4MConvP4M(64, 10, 1, stride=1, padding=0)
-        self.conv9_bn = nn.BatchNorm2d(10)
+        self.conv9_bn = nn.BatchNorm3d(10)
 
     def forward(self, x):
         x = F.dropout(x, p=0.2, training=True)
@@ -156,10 +150,13 @@ class P4MAllCNNC(nn.Module):
         x /= 8*8 *8
 
 
-        return F.log_softmax(x, dim=1) #is it right?
+        return F.log_softmax(x, dim=1)
 
-#model1 = AllCNNC()
+
+model1 = AllCNNC()
 model2 = P4AllCNNC()
+model3 = P4AllCNNC()
 input = torch.rand(10,3,32,32)
-#print("Model 1\n",model1(input).shape)
-print("Model 2 shape",model2(input).shape)
+print("Model 1 shape\n",model1(input).shape)
+print("Model 2 shape\n",model2(input).shape)
+print("Model 3 shape\n",model3(input).shape)
--- Models/ResNet.py
+++ Models/ResNet.py
@@ -1,178 +1,220 @@
-from torchvision.models.resnet import ResNet
-
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4
 from groupy.gconv.pytorch_gconv import P4MConvZ2, P4MConvP4M
-from groupy.gconv.pytorch_gconv.pooling import plane_group_spatial_max_pooling
-
-
-def conv3x3(layer, in_planes, out_planes, stride=1):
-
-    """3x3 convolution with padding"""
-    return layer(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
-def conv1x1(in_planes, out_planes, stride=1):
-
-    """1x1 convolution"""
-    return P4MConvP4M(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+from groupy.gconv.pytorch_gconv.pooling import plane_group_spatial_avg_pooling
 
 class BasicBlock(nn.Module):
-
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, base_width=64,
-                 conv_layer=None, norm_layer=None):
+    def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-        if conv_layer is None: conv_layer = P4MConvP4M
-        if norm_layer is None: norm_layer = nn.BatchNorm3d
-
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(conv_layer, inplanes, planes, stride)
-        self.bn1 = norm_layer(inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(conv_layer, planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        x = self.conv1(self.relu(self.bn1(x)))
-        x = self.conv2(self.relu(self.bn2(x)))
-
-        if self.downsample is not None:
-            identity = self.downsample(identity)
-
-        x += identity
-
-        return x
-
-class CustomResNet(ResNet):
-
-    def __init__(self, block, layers, channels, num_classes=1000, sep=None,
-                 zero_init_residual=False, width_per_group=64, norm_layer=None):
-        super(ResNet, self).__init__()
-
-        if norm_layer is None: norm_layer = nn.BatchNorm3d
-        self._norm_layer = norm_layer
-        conv_layer = {None: P4MConvP4M, 'g': gP4MConvP4M, 'gc': gcP4MConvP4M}
-
-        self.inplanes = channels[0]
-        self.base_width = width_per_group
-        self.conv1 = P4MConvZ2(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = norm_layer(channels[-1])
-        self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self._make_layer(block, channels[0], layers[0], conv_layer[sep])
-        self.layer2 = self._make_layer(block, channels[1], layers[1], conv_layer[sep], stride=2)
-        self.layer3 = self._make_layer(block, channels[2], layers[2], conv_layer[sep], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(channels[2] * block.expansion * 8, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch, so that the residual branch starts
-        # with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, conv_layer, stride=1):
-        norm_layer = self._norm_layer
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
+        self.shortcut = nn.Sequential() #identity
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
             )
 
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample,
-                            self.base_width, conv_layer, norm_layer))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, base_width=self.base_width,
-                                conv_layer=conv_layer, norm_layer=norm_layer))
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x):
-        x = self.conv1(x)
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        print(out.shape)
+        out = self.layer1(out)
+        print(out.shape)
+        out = self.layer2(out)
+        print(out.shape)
+        out = self.layer3(out)
+        print(out.shape)
+        out = F.avg_pool2d(out, out.size()[3])
+        print(out.shape)
+        out = out.view(out.size(0), -1)
+        print(out.shape)
+        out = self.linear(out)
+        return out
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
 
-        x = self.bn1(x)
-        x = self.relu(x)
+def ResNet44():
+    return ResNet(BasicBlock, [7, 7, 7])
 
-        n, nc, ns, nx, ny = x.shape
-        x = x.view(n,nc*ns,nx,ny)
 
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
 
-        return x
+
+
+class P4BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(P4BasicBlock, self).__init__()
+        self.conv1 = P4ConvP4(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm3d(planes)
+        self.conv2 = P4ConvP4(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm3d(planes)
+
+        self.shortcut = nn.Sequential() #identity
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                P4ConvP4(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm3d(self.expansion * planes)
+            )
 
     def forward(self, x):
-        return self._forward_impl(x)
-
-def p4mresnet44(**kwargs):
-    return CustomResNet(BasicBlock, [7,7,7], [11,23,45], **kwargs)
-
-def gp4mresnet44(**kwargs):
-    return CustomResNet(BasicBlock, [7,7,7], [19,38,76], sep='g', **kwargs)
-
-def gcp4mresnet44(**kwargs):
-    return CustomResNet(BasicBlock, [7,7,7], [28,56,112], sep='gc', **kwargs)
-
-def getMaxCudaMem(model, in_shape):
-    # Measure memory use
-    model = model.cuda()
-    criterion = nn.CrossEntropyLoss()
-    x = torch.rand(in_shape).cuda()
-    torch.cuda.reset_peak_memory_stats()
-    s = torch.cuda.max_memory_allocated()
-
-    # Perform forward + backward pass
-    y = m(x)
-    label = torch.tensor([0, 0]).cuda()
-    loss = criterion(y, label)
-    loss.backward()
-
-    mem = (torch.cuda.max_memory_allocated() - s) / 1024**2
-    print('Max memory used: {:.2f} MB'.format(mem))
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
-if __name__ == '__main__':
-    from torchinfo import summary
+class P4ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(P4ResNet, self).__init__()
+        self.in_planes = 8
 
-    m = p4mresnet44(num_classes=10)
-    print(m)
-    summary(m, (1,3,32,32), device='cpu', col_names=("input_size","output_size","num_params","kernel_size","mult_adds"))
-    getMaxCudaMem(m, (2,3,32,32))
+        self.conv1 = P4ConvZ2(3, 8, kernel_size=3, stride=1, padding=1, bias=False) #first convolution must be from Z2
+        self.bn1 = nn.BatchNorm3d(8)
+        self.layer1 = self._make_layer(block, 8, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 16, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 32, num_blocks[2], stride=2)
+        self.linear = nn.Linear(128, num_classes)
 
-    print('---')
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
 
-    m = gp4mresnet44(num_classes=10)
-    print(m)
-    summary(m, (1,3,32,32), device='cpu', col_names=("input_size","output_size","num_params","kernel_size","mult_adds"))
-    getMaxCudaMem(m, (2,3,32,32))
+        return nn.Sequential(*layers)
 
-    print('---')
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        print(out.shape)
+        out = self.layer1(out)
+        print(out.shape)
+        out = self.layer2(out)
+        print(out.shape)
+        out = self.layer3(out)
+        print(out.shape)
+        out = plane_group_spatial_avg_pooling(out, out.size()[4]) #in order to reduce filter to 1x1
+        print(out.shape)
+        out = out.view(out.size(0), -1)
+        print(out.shape)
+        out = self.linear(out)
+        return out
 
-    m = gcp4mresnet44(num_classes=10)
-    print(m)
-    summary(m, (1,3,32,32), device='cpu', col_names=("input_size","output_size","num_params","kernel_size","mult_adds"))
-    getMaxCudaMem(m, (2,3,32,32))
+
+def P4ResNet44():
+    return P4ResNet(P4BasicBlock, [7, 7, 7])
+
+
+
+
+
+class P4MBasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(P4MBasicBlock, self).__init__()
+        self.conv1 = P4MConvP4M(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm3d(planes)
+        self.conv2 = P4MConvP4M(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm3d(planes)
+
+        self.shortcut = nn.Sequential() #identity
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                P4MConvP4M(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm3d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class P4MResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(P4MResNet, self).__init__()
+        self.in_planes = 6
+
+        self.conv1 = P4MConvZ2(3, 6, kernel_size=3, stride=1, padding=1, bias=False) #first convolution must be from Z2
+        self.bn1 = nn.BatchNorm3d(6)
+        self.layer1 = self._make_layer(block, 6, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 11, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 23, num_blocks[2], stride=2)
+        self.linear = nn.Linear(184, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        print(out.shape)
+        out = self.layer1(out)
+        print(out.shape)
+        out = self.layer2(out)
+        print(out.shape)
+        out = self.layer3(out)
+        print(out.shape)
+        out = plane_group_spatial_avg_pooling(out, out.size()[4]) #in order to reduce filter to 1x1
+        print(out.shape)
+        out = out.view(out.size(0), -1)
+        print(out.shape)
+        out = self.linear(out)
+        return out
+
+
+def P4MResNet44():
+    return P4MResNet(P4MBasicBlock, [7, 7, 7])
+
+
+#model = ResNet44()
+#model = P4ResNet44()
+#model = P4MResNet44()
+#input = torch.rand(10,3,224,224)
+#print(model(input).shape)
