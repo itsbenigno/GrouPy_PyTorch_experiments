f3710cc3e627c14499b3f1ef17ecad7dd04cae0d
Benigno Ansanelli <47567968+itsbenigno@users.noreply.github.com>
2022-09-27 18:04:22 +0200

    Add files via upload

--- Models/CohenNet.py
+++ Models/CohenNet.py
@@ -119,3 +119,44 @@ class earlyP4CNN(nn.Module):
         x = torch.sum(x, dim=-1)
 
         return F.log_softmax(x, dim=1)
+
+
+
+
+
+
+class EP4CNN(nn.Module):
+    def __str__(self):
+        return "EP4CNN"
+
+    def __init__(self):
+        super(EP4CNN, self).__init__()
+        self.conv1 = P4ConvZ2(1, 10, kernel_size=3)
+        self.conv1_bn = nn.BatchNorm3d(10)
+        self.conv2 = P4ConvP4(10, 10, kernel_size=3)
+        self.conv2_bn = nn.BatchNorm3d(10)
+        self.conv3 = P4ConvP4(10, 10, kernel_size=3)
+        self.conv3_bn = nn.BatchNorm3d(10)
+        self.conv4 = P4ConvP4(10, 10, kernel_size=3)
+        self.conv4_bn = nn.BatchNorm3d(10)
+        self.conv5 = P4ConvP4(10, 10, kernel_size=3)
+        self.conv5_bn = nn.BatchNorm3d(10)
+        self.conv6 = P4ConvP4(10, 10, kernel_size=3)
+        self.conv6_bn = nn.BatchNorm3d(10)
+        self.conv7 = P4ConvP4(10, 10, kernel_size=4, padding=1)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = plane_group_spatial_max_pooling(x, 2, 2)
+        x = F.relu(self.conv3_bn(self.conv3(x)))
+        x = F.relu(self.conv4_bn(self.conv4(x)))
+        x = F.relu(self.conv5_bn(self.conv5(x)))
+        x = F.relu(self.conv6_bn(self.conv6(x)))
+        x = self.conv7(x)
+        x = torch.sum(x, dim=-3)
+        x = torch.sum(x, dim=-1)
+        x = torch.sum(x, dim=-1)
+
+        return F.log_softmax(x, dim=1)
