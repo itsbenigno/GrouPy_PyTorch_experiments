e75657b479edec8e99ba7e6417a72e213bf1185b
Benigno Ansanelli <47567968+itsbenigno@users.noreply.github.com>
2022-09-27 18:04:59 +0200

    Delete GrouPy directory

--- GrouPy/groupy/gconv/pytorch_gconv/__init__.py
+++
@@ -1,1 +0,0 @@
-from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4, P4MConvZ2, P4MConvP4M
--- GrouPy/groupy/gfunc/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from groupy.gfunc.p4func_array import P4FuncArray
-from groupy.gfunc.p4mfunc_array import P4MFuncArray
-from groupy.gfunc.z2func_array import Z2FuncArray
